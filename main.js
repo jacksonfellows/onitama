97bfa89e6f161db2f2413bac2d99082ff2e7de39
Jackson Fellows <jtfellows@icloud.com>
2024-08-16 11:19:25 -0700

    Better moving

--- main.js
+++ main.js
@@ -41,6 +41,12 @@ let css_class_lookup = {
     [WM]: "white_master"
 };
 
+function get_color(square_state) {
+    console.assert(square_state != EMPTY);
+    if (square_state == BP || square_state == BM) return "black";
+    return "white";
+}
+
 function draw_board(board) {
     for (let row = 0; row < 5; row++) {
         for (let col = 0; col < 5; col++) {
@@ -65,7 +71,7 @@ let game_state = {
 draw_board(game_state.board);
 
 function handle_move(start_row, start_col, end_row, end_col) {
-    let valid_moves = get_valid_moves(start_row, start_col, "monkey");
+    let valid_moves = get_valid_moves(start_row, start_col, "rooster");
     if (valid_moves.map(([row, col]) => row == end_row && col == end_col).reduce((x, y) => x || y)) {
         game_state.board[end_row][end_col] = game_state.board[start_row][start_col];
         game_state.board[start_row][start_col] = EMPTY;
@@ -79,6 +85,20 @@ let move_cards = {
         [0, 1, 0, 1, 0],
         [0, 0, 0, 0, 0],
         [0, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0]
+    ],
+    dragon: [
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0]
+    ],
+    rooster: [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0],
+        [0, 1, 0, 1, 0],
+        [0, 1, 0, 0, 0],
         [0, 0, 0, 0, 0]
     ]
 };
@@ -101,17 +121,26 @@ function on_board(row, col) {
 }
 
 function get_valid_moves(row, col, card_name) {
+    let color = get_color(game_state.board[row][col])
     let moves = [];
     for (let [drow, dcol] of move_cards_offsets[card_name]) {
+        if (color == "black") {
+            // Flip moves.
+            drow = -drow;
+            dcol = -dcol;
+        }
         if (on_board(row + drow, col + dcol)) {
-            moves.push([row + drow, col + dcol]);
+            let dst_square = game_state.board[row + drow][col + dcol];
+            if (dst_square == EMPTY || get_color(dst_square) != color) {
+                moves.push([row + drow, col + dcol]);
+            }
         }
     }
     return moves;
 }
 
 function handle_move_attempt_start(row, col) {
-    let valid_moves = get_valid_moves(row, col, "monkey");
+    let valid_moves = get_valid_moves(row, col, "rooster");
     for (let [move_row, move_col] of valid_moves) {
         board_divs[move_row][move_col].classList.add("valid-move");
     }
