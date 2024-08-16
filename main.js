e7071f6dc0cdd528416d7c7f0d3a9e55ec2ca4d7
Jackson Fellows <jtfellows@icloud.com>
2024-08-16 11:07:15 -0700

    Basic move card

--- main.js
+++ main.js
@@ -12,9 +12,13 @@ let start_row_col = null;
 for (let row = 0; row < 5; row++) {
     for (let col = 0; col < 5; col++) {
         board_divs[row][col].addEventListener("mousedown", _ => {
-            start_row_col = [row, col];
+            if (game_state.board[row][col] != EMPTY) {
+                handle_move_attempt_start(row, col);
+                start_row_col = [row, col];
+            }
         });
         board_divs[row][col].addEventListener("mouseup", _ => {
+            handle_move_attempt_end();
             [start_row, start_col] = start_row_col;
             if (start_row != row || start_col != col) {
                 handle_move(start_row, start_col, row, col);
@@ -61,7 +65,62 @@ let game_state = {
 draw_board(game_state.board);
 
 function handle_move(start_row, start_col, end_row, end_col) {
-    game_state.board[end_row][end_col] = game_state.board[start_row][start_col];
-    game_state.board[start_row][start_col] = EMPTY;
-    draw_board(game_state.board);
+    let valid_moves = get_valid_moves(start_row, start_col, "monkey");
+    if (valid_moves.map(([row, col]) => row == end_row && col == end_col).reduce((x, y) => x || y)) {
+        game_state.board[end_row][end_col] = game_state.board[start_row][start_col];
+        game_state.board[start_row][start_col] = EMPTY;
+        draw_board(game_state.board);
+    }
+}
+
+let move_cards = {
+    monkey: [
+        [0, 0, 0, 0, 0],
+        [0, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0],
+        [0, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0]
+    ]
+};
+
+let move_cards_offsets = {};
+for (let card_name in move_cards) {
+    let offsets = [];
+    for (let row = 0; row < 5; row++) {
+        for (let col = 0; col < 5; col++) {
+            if (move_cards[card_name][row][col]) {
+                offsets.push([row - 2, col - 2])
+            }
+        }
+    }
+    move_cards_offsets[card_name] = offsets;
+}
+
+function on_board(row, col) {
+    return 0 <= row && row < 5 && 0 <= col && col < 5;
+}
+
+function get_valid_moves(row, col, card_name) {
+    let moves = [];
+    for (let [drow, dcol] of move_cards_offsets[card_name]) {
+        if (on_board(row + drow, col + dcol)) {
+            moves.push([row + drow, col + dcol]);
+        }
+    }
+    return moves;
+}
+
+function handle_move_attempt_start(row, col) {
+    let valid_moves = get_valid_moves(row, col, "monkey");
+    for (let [move_row, move_col] of valid_moves) {
+        board_divs[move_row][move_col].classList.add("valid-move");
+    }
+}
+
+function handle_move_attempt_end() {
+    for (let row = 0; row < 5; row++) {
+        for (let col = 0; col < 5; col++) {
+            board_divs[row][col].classList.remove("valid-move");
+        }
+    }
 }
